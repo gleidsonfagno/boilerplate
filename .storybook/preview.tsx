bfaae0fdca8c6d275f2050b6cae366161f1ec4e9
gledsonfagno <gleidsonfagner838@gmail.com>
2024-05-18 17:03:45 -0300

    Mensagem do commit

---
+++ .storybook/preview.tsx
@@ -0,0 +1,25 @@
+// import type { Preview } from '@storybook/react'
+
+// const preview: Preview = {
+//   parameters: {
+//     controls: {
+//       matchers: {
+//         color: /(background|color)$/i,
+//         date: /Date$/i
+//       }
+//     }
+//   }
+// }
+
+// export default preview
+
+import React from 'react'
+import GlobalStyles from '../src/styles/global'
+export const decorators = [
+  (Story) => (
+    <>
+    <GlobalStyles />
+    <Story />
+    </>
+  )
+]
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+// import React from 'react'
+// import Main from '.'
+
+// export default {
+//   title: 'Main',
+//   component: Main
+// }
+
+// export const Basic = () => <Main />
+
+import React from 'react'
+import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
+import { Meta, Story } from '@storybook/react'
 
 export default {
   title: 'Main',
-  component: Main
-}
+  component: Main,
+  decorators: [withKnobs]
+} as Meta
 
-export const Basic = () => <Main />
+export const Basic: Story = () => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text(
+      'Description',
+      'TypeScript, ReactJS, NextJS e Styled Components'
+    )}
+  />
+)
